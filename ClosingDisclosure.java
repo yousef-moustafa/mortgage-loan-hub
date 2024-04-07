566d38dd52c3334ae169ec01ed7439f81c0cf899
Kareemahmed182 <109769452+Kareemahmed182@users.noreply.github.com>
2024-04-07 20:32:20 +0200

    loanestimate & ClosingDisclosure

--- ClosingDisclosure.java
+++ ClosingDisclosure.java
@@ -1,5 +1,3 @@
-public class ClosingDisclosure {
-
     // create a constructor to initialize ClosingDisclosure's objects.
     // and a list of methods to get and retrieve ClosingDisclosure's information.
     // ClosingDisclosure usually have the following type of information
@@ -11,5 +9,44 @@ public class ClosingDisclosure {
     * */
 
     // Define methods to calculate ClosingCosts based on the loan amount
+import java.time.LocalDate;
 
+public class ClosingDisclosure {
+    private String loanId;
+    private LocalDate closingDate;
+    private double closingCosts;
+    
+    public ClosingDisclosure(String loanId, LocalDate closingDate) {
+        this.loanId = loanId;
+        this.closingDate = closingDate;
+        calculateClosingCosts();
+    }
+
+    public String getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(String loanId) {
+        this.loanId = loanId;
+        calculateClosingCosts();
+    }
+
+    public LocalDate getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(LocalDate closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public double getClosingCosts() {
+        return closingCosts;
+    }
+
+    private void calculateClosingCosts() {
+        double percentage = Math.random() * (0.06 - 0.02) + 0.02;
+        double loanAmount = 200000;
+        closingCosts = loanAmount * percentage;
+    }
+    
 }
--- LoanEstimate.java
+++ LoanEstimate.java
@@ -1,20 +1,90 @@
 public class LoanEstimate {
-    // create a constructor to initialize LoanEstimate's objects.
-    // and a list of methods to get and retrieve LoanEstimate's information.
-    // LoanEstimate usually have the following type of information
+    private String applicantName;
+    private double loanAmount;
+    private int loanTerm;
+    private double interestRate;
+    private double monthlyPrincipalAndInterest;
+    private double mortgageInsurance;
+    private double estimatedEscrow;
+    private double estimatedTotalMonthlyPayment;
 
-    /*
-    Applicant_Name (the Borrower's name)
-    Loan_amount
-    Loan_Term
-    Interest_rate
-    Monthly_Principal_AND_Interest
-    Mortgage_Insurance
-    Estimated_Escrow
-    Estimated_Total_Monthly_Payment (Monthly_Principal_AND_Interest + Mortgage_Insurance + Estimated_Escrow)
-    * */
+    public LoanEstimate(String applicantName, double loanAmount, int loanTerm, double interestRate) {
+        this.applicantName = applicantName;
+        this.loanAmount = loanAmount;
+        this.loanTerm = loanTerm;
+        this.interestRate = interestRate;
+        calculateMonthlyPrincipalAndInterest();
+    }
 
-    // Define a method that calculates the Estimated_Total_Monthly_Payment based on the information
-    // of Loan_amount, Loan_Term, Interest_rate
+    public void calculateEstimatedTotalMonthlyPayment() {
+        estimatedTotalMonthlyPayment = monthlyPrincipalAndInterest + mortgageInsurance + estimatedEscrow;
+    }
 
+    public String getApplicantName() {
+        return applicantName;
+    }
+
+    public void setApplicantName(String applicantName) {
+        this.applicantName = applicantName;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+        calculateMonthlyPrincipalAndInterest();
+    }
+
+    public int getLoanTerm() {
+        return loanTerm;
+    }
+
+    public void setLoanTerm(int loanTerm) {
+        this.loanTerm = loanTerm;
+        calculateMonthlyPrincipalAndInterest();
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+        calculateMonthlyPrincipalAndInterest();
+    }
+
+    public double getMonthlyPrincipalAndInterest() {
+        return monthlyPrincipalAndInterest;
+    }
+
+    public double getMortgageInsurance() {
+        return mortgageInsurance;
+    }
+
+    public void setMortgageInsurance(double mortgageInsurance) {
+        this.mortgageInsurance = mortgageInsurance;
+        calculateEstimatedTotalMonthlyPayment();
+    }
+
+    public double getEstimatedEscrow() {
+        return estimatedEscrow;
+    }
+
+    public void setEstimatedEscrow(double estimatedEscrow) {
+        this.estimatedEscrow = estimatedEscrow;
+        calculateEstimatedTotalMonthlyPayment();
+    }
+
+    public double getEstimatedTotalMonthlyPayment() {
+        return estimatedTotalMonthlyPayment;
+    }
+    
+    private void calculateMonthlyPrincipalAndInterest() {
+        int months = loanTerm * 12;
+        double monthlyInterestRate = interestRate / 100 / 12;
+        monthlyPrincipalAndInterest = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -months));
+    }
+    
 }
