ecaacc506b1b84e5ba755a16ab4dd26e8e85c6a1
mohamedhelmy21 <mohamed.helmy@gaf.ac>
2024-04-08 17:21:59 +0200

    mortgage market class

--- MortgageMarket.java
+++ MortgageMarket.java
@@ -1,12 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MortgageMarket {
-    // Create a constructor to initialize MortgageMarket' objects.
-    // Create a list of methods to get and retrieve MortgageMarket's information.
-    // MortgageMarket usually has the following type of information:
+    private List<Borrower> borrowers;
+    private List<Lender> lenders;
+    private List<Property> properties;
 
-    /*
-    * Borrowers
-    * Lenders
-    * Properties
-    * */
+    // Constructor
+    public MortgageMarket() {
+        borrowers = new ArrayList<>();
+        lenders = new ArrayList<>();
+        properties = new ArrayList<>();
+    }
 
+    // Method to add a borrower
+    public void addBorrower(Borrower borrower) {
+        borrowers.add(borrower);
+    }
+
+    // Method to add a lender
+    public void addLender(Lender lender) {
+        lenders.add(lender);
+    }
+
+    // Method to add a property
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+
+    // Method to retrieve all borrowers
+    public List<Borrower> getBorrowers() {
+        return borrowers;
+    }
+
+    // Method to retrieve all lenders
+    public List<Lender> getLenders() {
+        return lenders;
+    }
+
+    // Method to retrieve all properties
+    public List<Property> getProperties() {
+        return properties;
+    }
 }
